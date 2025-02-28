d26907b9adc10199da0e0a43cfb1d746b004f1d8
eyasin-mnrs <eyasin@minres.com>
2025-02-28 12:57:28 +0100

    Added the dma module

---
+++ minres/dma.cpp
@@ -0,0 +1,185 @@
+#include "dma.h"
+#include <scc/report.h>
+#include <scc/utilities.h>
+
+using namespace std;
+using namespace sc_core;
+
+namespace vpvper {
+namespace minres {
+
+SC_HAS_PROCESS(dma);
+
+dma::dma(sc_core::sc_module_name nm)
+: sc_core::sc_module(nm)
+, scc::tlm_target<>(clk_period)
+, initiator_socket("initiator_socket")
+, NAMEDD(regs, dma_regs)
+
+{
+    regs->control.set_write_cb([this](scc::sc_register<uint32_t>& reg, uint32_t data) -> bool {
+        bool prev_ch0 = regs->r_control.ch0_enable_transfer;
+        bool prev_ch1 = regs->r_control.ch1_enable_transfer;
+
+        reg.put(data); // Write new value to register
+
+        bool new_ch0 = regs->r_control.ch0_enable_transfer;
+        bool new_ch1 = regs->r_control.ch1_enable_transfer;
+
+        if(prev_ch0 == 0 && new_ch0 == 1) {
+            ch0_enable_event.notify(SC_ZERO_TIME); // Trigger event immediately
+        }
+
+        if(prev_ch1 == 0 && new_ch1 == 1) {
+            ch1_enable_event.notify(SC_ZERO_TIME); // Trigger event immediately
+        }
+
+        return true;
+    });
+
+    regs->registerResources(*this);
+
+    SC_METHOD(reset_cb);
+    sensitive << rst_i;
+    dont_initialize();
+
+    // SC_THREAD(update_signal);
+    SC_THREAD(transfer_process_ch0);
+    SC_THREAD(transfer_process_ch1);
+
+    // Update IRQ when interrupt pending registers change
+    regs->ip.set_write_cb([this](scc::sc_register<uint32_t>&, uint32_t, sc_time) -> bool {
+        update_irq();
+        return true;
+    });
+}
+
+dma::~dma() = default;
+
+void dma::reset_cb() {
+    if(rst_i.read()) {
+        regs->reset_start();
+        irq_o.write(false);
+    } else {
+        regs->reset_stop();
+    }
+}
+
+void dma::transfer_process_ch0() {
+    while(true) {
+        // Wait for channel 0 enable and event trigger
+        wait(ch0_enable_event);
+
+        if(regs->r_control.ch0_enable_transfer && !regs->r_status.ch0_busy) {
+            regs->r_status.ch0_busy = 1;
+            uint32_t src_addr = regs->r_ch0_src_start_addr;
+            uint32_t dst_addr = regs->r_ch0_dst_start_addr;
+            uint32_t seg_length = regs->r_ch0_transfer.seg_length;
+            uint32_t seg_count = regs->r_ch0_transfer.seg_count;
+
+            for(uint32_t seg = 0; seg < seg_count; seg++) {
+                // Transfer data for this segment
+                for(uint32_t i = 0; i < seg_length; i++) {
+                    tlm::tlm_generic_payload trans;
+                    sc_time delay = SC_ZERO_TIME;
+                    uint8_t data;
+
+                    // Read from source
+                    trans.set_command(tlm::TLM_READ_COMMAND);
+                    trans.set_address(src_addr);
+                    trans.set_data_ptr(&data);
+                    trans.set_data_length(1);
+                    initiator_socket->b_transport(trans, delay);
+
+                    // Write to destination
+                    trans.set_command(tlm::TLM_WRITE_COMMAND);
+                    trans.set_address(dst_addr);
+                    initiator_socket->b_transport(trans, delay);
+
+                    // Update addresses based on step (per-byte increment)
+                    src_addr += regs->r_ch0_src_addr_inc.src_step;
+                    dst_addr += regs->r_ch0_dst_addr_inc.dst_step;
+                }
+
+                // Update addresses with stride after each segment
+                src_addr += regs->r_ch0_src_addr_inc.src_stride;
+                dst_addr += regs->r_ch0_dst_addr_inc.dst_stride;
+
+                // Trigger segment done interrupt
+                if(regs->r_ie.ch0_ie_seg_transfer_done) {
+                    regs->r_ip.ch0_ip_seg_transfer_done = 1;
+                    update_irq();
+                }
+            }
+
+            regs->r_status.ch0_busy = 0;
+            if(regs->r_ie.ch0_ie_transfer_done) {
+                regs->r_ip.ch0_ip_transfer_done = 1;
+                update_irq();
+            }
+        }
+    }
+}
+
+void dma::transfer_process_ch1() {
+    // Similar logic for Channel 1
+    while(true) {
+
+        wait(ch1_enable_event);
+
+        if(regs->r_control.ch1_enable_transfer && !regs->r_status.ch1_busy) {
+            regs->r_status.ch1_busy = 1;
+            uint32_t src_addr = regs->r_ch1_src_start_addr;
+            uint32_t dst_addr = regs->r_ch1_dst_start_addr;
+            uint32_t seg_length = regs->r_ch1_transfer.seg_length;
+            uint32_t seg_count = regs->r_ch1_transfer.seg_count;
+
+            for(uint32_t seg = 0; seg < seg_count; seg++) {
+
+                for(uint32_t i = 0; i < seg_length; i++) {
+                    tlm::tlm_generic_payload trans;
+                    sc_time delay = SC_ZERO_TIME;
+                    uint8_t data;
+
+                    trans.set_command(tlm::TLM_READ_COMMAND);
+                    trans.set_address(src_addr);
+                    trans.set_data_ptr(&data);
+                    trans.set_data_length(1);
+                    initiator_socket->b_transport(trans, delay);
+
+                    trans.set_command(tlm::TLM_WRITE_COMMAND);
+                    trans.set_address(dst_addr);
+                    initiator_socket->b_transport(trans, delay);
+
+                    src_addr += regs->r_ch1_src_addr_inc.src_step;
+                    dst_addr += regs->r_ch1_dst_addr_inc.dst_step;
+                }
+
+                src_addr += regs->r_ch1_src_addr_inc.src_stride;
+                dst_addr += regs->r_ch1_dst_addr_inc.dst_stride;
+
+                if(regs->r_ie.ch1_ie_seg_transfer_done) {
+                    regs->r_ip.ch1_ip_seg_transfer_done = 1;
+                    update_irq();
+                }
+            }
+
+            regs->r_status.ch1_busy = 0;
+            if(regs->r_ie.ch1_ie_transfer_done) {
+                regs->r_ip.ch1_ip_transfer_done = 1;
+                update_irq();
+            }
+        }
+    }
+}
+
+void dma::update_irq() {
+    bool irq = (regs->r_ip.ch0_ip_seg_transfer_done && regs->r_ie.ch0_ie_seg_transfer_done) ||
+               (regs->r_ip.ch0_ip_transfer_done && regs->r_ie.ch0_ie_transfer_done) ||
+               (regs->r_ip.ch1_ip_seg_transfer_done && regs->r_ie.ch1_ie_seg_transfer_done) ||
+               (regs->r_ip.ch1_ip_transfer_done && regs->r_ie.ch1_ie_transfer_done);
+    irq_o.write(irq);
+}
+
+} // namespace minres
+} // namespace vpvper
---
+++ minres/dma.h
@@ -0,0 +1,47 @@
+#ifndef DMA_H
+#define DMA_H
+
+#include "gen/dma_regs.h"
+#include "tlm/scc/initiator_mixin.h"
+#include <cci_configuration>
+#include <scc/clock_if_mixins.h>
+#include <scc/tlm_target.h>
+#include <tlm.h>
+#include <tlm_utils/simple_initiator_socket.h>
+#include <tlm_utils/simple_target_socket.h>
+
+namespace vpvper {
+namespace minres {
+
+class dma : public sc_core::sc_module, public scc::tlm_target<> {
+public:
+    sc_core::sc_in<bool> rst_i{"rst_i"};     // Reset signal
+    sc_core::sc_out<bool> irq_o{"irq_o"};    // Interrupt output
+    sc_core::sc_event ch0_enable_event;      // Event for register bit
+    sc_core::sc_event ch1_enable_event;      // Event for register bit
+
+    // TLM Sockets
+    tlm_utils::simple_initiator_socket<dma, scc::LT> initiator_socket;
+
+    dma(sc_core::sc_module_name nm);
+    virtual ~dma() override;
+
+    void set_clock_period(sc_core::sc_time period) { clk_period = period; }
+
+protected:
+    void reset_cb();
+    void transfer_process_ch0();
+    void transfer_process_ch1();
+    void update_irq();
+
+    sc_core::sc_time clk_period;
+    std::unique_ptr<dma_regs> regs;
+};
+
+using dma_tl = scc::tickless_clock<dma>;
+using dma_tc = scc::ticking_clock<dma>;
+
+} // namespace minres
+} // namespace vpvper
+
+#endif // DMA_H
