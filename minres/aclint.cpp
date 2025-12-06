b88c732cb14ee8c21a448737f6262080817b8af9
Eyck Jentzsch <eyck@minres.com>
2025-12-06 21:08:42 +0100

    simplifies aclint registers

--- minres/aclint.cpp
+++ minres/aclint.cpp
@@ -20,27 +20,20 @@ aclint::aclint(sc_module_name nm, size_t num_cpus)
 
     SC_METHOD(reset_cb);
     sensitive << rst_i;
-    regs->mtime_hi.set_read_cb([this](const sc_register<uint32_t>& reg, uint32_t& data, sc_time& d) -> bool {
+    regs->mtime.set_read_cb([this](const sc_register<uint64_t>& reg, uint64_t& data, sc_time& d) -> bool {
         uint64_t elapsed_clks = mtime_clk_period.value() ? (sc_time_stamp() + d - last_updt) / mtime_clk_period : 0;
-        data = regs->in_reset() ? 0 : regs->r_mtime_hi + (elapsed_clks >> 32);
+        data = regs->in_reset() ? 0 : regs->r_mtime + elapsed_clks;
         return true;
     });
-    regs->mtime_lo.set_read_cb([this](const sc_register<uint32_t>& reg, uint32_t& data, sc_time& d) -> bool {
-        uint64_t elapsed_clks = mtime_clk_period.value() ? (sc_time_stamp() + d - last_updt) / mtime_clk_period : 0;
-        data = regs->in_reset() ? 0 : regs->r_mtime_lo + elapsed_clks;
-        return true;
-    });
-    auto write_cb = [this](const sc_register<uint32_t>& reg, const uint32_t data, sc_time& d) -> bool {
+    regs->mtime.set_write_cb([this](const sc_register<uint64_t>& reg, const uint64_t data, sc_time& d) -> bool {
         if(!regs->in_reset()) {
             reg.put(data);
             this->update_mtime();
             return true;
         }
         return false;
-    };
-    regs->mtime_hi.set_write_cb(write_cb);
-    regs->mtime_lo.set_write_cb(write_cb);
-    auto write_cb_with_wait = [this](const sc_register<uint32_t>& reg, const uint32_t data, sc_time& d) -> bool {
+    });
+    auto write32_cb_with_wait = [this](const sc_register<uint32_t>& reg, const uint32_t data, sc_time& d) -> bool {
         if(d.value())
             wait(d);
         if(!regs->in_reset()) {
@@ -50,10 +43,19 @@ aclint::aclint(sc_module_name nm, size_t num_cpus)
         }
         return false;
     };
-    for(auto i = 0u; i < regs->mtimecmphi.size(); ++i) {
-        regs->mtimecmphi[i].set_write_cb(write_cb_with_wait);
-        regs->mtimecmplo[i].set_write_cb(write_cb_with_wait);
-        regs->msip[i].set_write_cb(write_cb_with_wait);
+    auto write64_cb_with_wait = [this](const sc_register<uint64_t>& reg, const uint64_t data, sc_time& d) -> bool {
+        if(d.value())
+            wait(d);
+        if(!regs->in_reset()) {
+            reg.put(data);
+            this->update_mtime();
+            return true;
+        }
+        return false;
+    };
+    for(auto i = 0u; i < regs->mtimecmp.size(); ++i) {
+        regs->mtimecmp[i].set_write_cb(write64_cb_with_wait);
+        regs->msip[i].set_write_cb(write32_cb_with_wait);
     }
     SC_METHOD(update_mtime);
     sensitive << mtime_evt;
@@ -72,32 +74,28 @@ void aclint::reset_cb() {
 
 void aclint::update_mtime() {
     if(mtime_clk_period > SC_ZERO_TIME) {
-        uint64_t mtime = static_cast<uint64_t>(regs->r_mtime_hi) << 32 | regs->r_mtime_lo;
         // update mtime register
         uint64_t elapsed_clks = (sc_time_stamp() - last_updt) / mtime_clk_period;
         last_updt += elapsed_clks * mtime_clk_period;
         if(elapsed_clks) {
-            mtime += elapsed_clks;
-            regs->mtime_hi = mtime >> 32;
-            regs->mtime_lo = static_cast<uint32_t>(mtime);
+            regs->r_mtime += elapsed_clks;
             if(mtime_o.get_interface())
-                mtime_o->write(mtime);
+                mtime_o->write(regs->r_mtime);
         }
         // check all mtimecmp register if there is an interrupt
         uint64_t mtimecmp_min = std::numeric_limits<uint64_t>::max();
-        for(auto i = 0u; i < regs->mtimecmphi.size(); ++i) {
-            uint64_t mtimecmp = static_cast<uint64_t>(regs->r_mtimecmphi[i]) << 32 | regs->r_mtimecmplo[i];
+        for(auto i = 0u; i < regs->mtimecmp.size(); ++i) {
             // check for and handle interrupts
             uint64_t smallest = std::numeric_limits<uint64_t>::max();
-            mtime_int_o[i].write(mtimecmp <= mtime);
-            mtimecmp_min = std::min(mtimecmp_min, mtimecmp);
+            mtime_int_o[i].write(regs->r_mtimecmp[i] <= regs->r_mtime);
+            mtimecmp_min = std::min(mtimecmp_min, regs->r_mtimecmp[i]);
         }
         // calculate next invocation
         mtime_evt.cancel();
         if(mtime_o.get_interface()) {
             mtime_evt.notify(mtime_clk_period);
-        } else if(mtimecmp_min > mtime) {
-            sc_time nexttrigger = mtime_clk_period * (mtimecmp_min - mtime);
+        } else if(mtimecmp_min > regs->r_mtime) {
+            sc_time nexttrigger = mtime_clk_period * (mtimecmp_min - regs->r_mtime);
             mtime_evt.notify(nexttrigger);
         }
     }
--- minres/gen/aclint_regs.h
+++ minres/gen/aclint_regs.h
@@ -30,23 +30,17 @@ public:
     END_BF_DECL();
     std::vector<msip0_t> r_msip;
 
-    std::vector<uint32_t> r_mtimecmplo;
+    std::vector<uint64_t> r_mtimecmp;
 
-    std::vector<uint32_t> r_mtimecmphi;
-
-    uint32_t r_mtime_lo;
-
-    uint32_t r_mtime_hi;
+    uint64_t r_mtime;
 
     //////////////////////////////////////////////////////////////////////////////
     // register declarations
     //////////////////////////////////////////////////////////////////////////////
 
     sc_core::sc_vector<scc::sc_register<msip0_t>> msip;
-    sc_core::sc_vector<scc::sc_register<uint32_t>> mtimecmplo;
-    sc_core::sc_vector<scc::sc_register<uint32_t>> mtimecmphi;
-    scc::sc_register<uint32_t> mtime_lo;
-    scc::sc_register<uint32_t> mtime_hi;
+    sc_core::sc_vector<scc::sc_register<uint64_t>> mtimecmp;
+    scc::sc_register<uint64_t> mtime;
 
     aclint_regs(sc_core::sc_module_name nm, size_t num_cpus);
 
@@ -60,31 +54,22 @@ public:
 inline aclint_regs::aclint_regs(sc_core::sc_module_name nm, size_t num_cpus)
 : sc_core::sc_module(nm)
 , r_msip(num_cpus)
-, r_mtimecmplo(num_cpus)
-, r_mtimecmphi(num_cpus)
+, r_mtimecmp(num_cpus)
 , msip("msip", num_cpus, [this](char const* name, int idx) { return new scc::sc_register<msip0_t>(name, r_msip[idx], 0, *this); })
-, mtimecmplo("mtimecmplo", num_cpus,
-             [this](char const* name, int idx) {
-                 return new scc::sc_register<msip0_t>(name, r_mtimecmplo[idx], std::numeric_limits<uint32_t>::max(), *this);
-             })
-, mtimecmphi("mtimecmphi", num_cpus,
-             [this](char const* name, int idx) {
-                 return new scc::sc_register<msip0_t>(name, r_mtimecmphi[idx], std::numeric_limits<uint32_t>::max(), *this);
-             })
-, NAMED(mtime_lo, r_mtime_lo, 0, *this)
-, NAMED(mtime_hi, r_mtime_hi, 0, *this) {}
+, mtimecmp("mtimecmp", num_cpus,
+           [this](char const* name, int idx) {
+               return new scc::sc_register<uint64_t>(name, r_mtimecmp[idx], std::numeric_limits<uint64_t>::max(), *this);
+           })
+, NAMED(mtime, r_mtime, 0, *this) {}
 
 template <unsigned BUSWIDTH> inline void aclint_regs::registerResources(scc::tlm_target<BUSWIDTH>& target) {
-    const size_t msie_sz = sizeof(decltype(r_msip)::value_type::backing);
-    const size_t mtimecmplo_sz = sizeof(decltype(r_mtimecmplo)::value_type);
-    const size_t mtimecmphi_sz = sizeof(decltype(r_mtimecmphi)::value_type);
-    for(auto i = 0u; i < mtimecmplo.size(); ++i) {
+    constexpr size_t msie_sz = sizeof(decltype(r_msip)::value_type::backing);
+    constexpr size_t mtimecmp_sz = sizeof(decltype(r_mtimecmp)::value_type);
+    for(auto i = 0u; i < mtimecmp.size(); ++i) {
         target.addResource(msip[i], 0x0UL + msie_sz * i);
-        target.addResource(mtimecmplo[i], 0x4000UL + (mtimecmplo_sz + mtimecmphi_sz) * i);
-        target.addResource(mtimecmphi[i], 0x4000UL + mtimecmplo_sz + (mtimecmplo_sz + mtimecmphi_sz) * i);
+        target.addResource(mtimecmp[i], 0x4000UL + mtimecmp_sz * i);
     }
-    target.addResource(mtime_lo, 0xbff8UL);
-    target.addResource(mtime_hi, 0xbffcUL);
+    target.addResource(mtime, 0xbff8UL);
 }
 } // namespace minres
 } // namespace vpvper
